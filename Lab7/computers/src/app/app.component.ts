232986c01fa295eb457d10e61fa6d8791c165865
William <williamdmoore2002@gmail.com>
2024-11-11 00:05:31 +0000

    Working Lab7

--- Lab7/computers/src/app/app.component.ts
+++ Lab7/computers/src/app/app.component.ts
@@ -1,21 +1,24 @@
 import {Component} from '@angular/core';
 import {PcComponent} from './pc/pc.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PcComponent],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
-        <app-pc></app-pc>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
+  imports: [PcComponent, RouterModule],
 })
 export class AppComponent {
   title = 'pc';
